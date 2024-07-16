2e63230046df6626842bc6ac3f7e90224cf08e31
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-07-16 10:05:13 +0200

    Configuration

---
+++ Unitutor/src/main/java/marinalucentini/Unitutor/security/Config.java
@@ -0,0 +1,53 @@
+package marinalucentini.Unitutor.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class Config {
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+    @Value("${cors.allowed.origins2}")
+    private String allowedOrigins2;
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.formLogin(http -> http.disable());
+        httpSecurity.csrf(http -> http.disable());
+        httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.cors(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+        return httpSecurity.build();
+
+    }
+    @Bean
+    PasswordEncoder getBCrypt(){
+        return new BCryptPasswordEncoder(10);
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins, allowedOrigins2));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
