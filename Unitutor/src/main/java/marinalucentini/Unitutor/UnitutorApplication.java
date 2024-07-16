db7730398191d70f2107b3d2ac50d19b6d37eb6c
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-07-16 07:18:03 +0200

    Configuration with dependency

---
+++ Unitutor/src/main/java/marinalucentini/Unitutor/UnitutorApplication.java
@@ -0,0 +1,13 @@
+package marinalucentini.Unitutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitutorApplication.class, args);
+	}
+
+}
---
+++ Unitutor/src/test/java/marinalucentini/Unitutor/UnitutorApplicationTests.java
@@ -0,0 +1,13 @@
+package marinalucentini.Unitutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
