9541082cb6d1bbffd172599ef19156eff3ded550
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-08-05 11:41:30 +0200

    update and delete transcription

---
+++ Unitutor/react/src/component/Transcription/TranscriptionDeleteModal.jsx
@@ -0,0 +1,34 @@
+import { Button, Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { transcriptionDeleteFunction } from "../../redux/actions/transcription";
+
+const TranscriptionDeleteModal = ({ show, handleClose, subjectId, TranscriptionId }) => {
+  const { error } = useSelector((state) => state.transcription);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(transcriptionDeleteFunction(subjectId, TranscriptionId));
+    if (!error) {
+      navigate("/profile");
+    }
+  };
+  return (
+    <>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sei sicuro di voler eliminare la materia ?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Button variant="secondary" onClick={handleClose} className="mx-3">
+            Torna indietro
+          </Button>
+          <Button variant="primary" onClick={handleDelete}>
+            Elimina
+          </Button>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+export default TranscriptionDeleteModal;
